998e0dd9daa0bd61dfe873844edaccbfdec6e085
Argha Das <arghadasofficial@gmai.com>
2024-05-14 17:23:13 +0530

    Added Speed Converter Tool

--- app/src/main/java/com/scriptintech/unitconverterx/repository/speed/SpeedImpl.kt
+++ app/src/main/java/com/scriptintech/unitconverterx/repository/speed/SpeedImpl.kt
@@ -4,6 +4,7 @@ import com.scriptintech.unitconverterx.components.model.UDropdownModel
 import com.scriptintech.unitconverterx.repository.ConverterDefault
 
 class SpeedImpl : Speed, ConverterDefault {
+
     override fun convertStringToDouble(input: String): Double {
         return input.toDoubleOrNull() ?: 0.0
     }
@@ -19,28 +20,32 @@ class SpeedImpl : Speed, ConverterDefault {
     }
 
     override fun calculate(keyFrom: String, keyTo: String, value: Double): Double {
-        return when (keyTo) {
-            getUnits()[0].unit -> {
-                toMilePerHour(fromKey = keyFrom, value)
-            }
+        return if (keyFrom == keyTo) {
+            value
+        } else {
+            when (keyTo) {
+                getUnits()[0].unit -> {
+                    toMilePerHour(fromKey = keyFrom, value)
+                }
 
-            getUnits()[1].unit -> {
-                toFootPerSecond(fromKey = keyFrom, value)
-            }
+                getUnits()[1].unit -> {
+                    toFootPerSecond(fromKey = keyFrom, value)
+                }
 
-            getUnits()[2].unit -> {
-                toMeterPerSecond(fromKey = keyFrom, value)
-            }
+                getUnits()[2].unit -> {
+                    toMeterPerSecond(fromKey = keyFrom, value)
+                }
 
-            getUnits()[3].unit -> {
-                toKilometerPerHour(fromKey = keyFrom, value)
-            }
+                getUnits()[3].unit -> {
+                    toKilometerPerHour(fromKey = keyFrom, value)
+                }
 
-            getUnits()[4].unit -> {
-                toKnot(fromKey = keyFrom, value)
-            }
+                getUnits()[4].unit -> {
+                    toKnot(fromKey = keyFrom, value)
+                }
 
-            else -> 0.0
+                else -> 0.0
+            }
         }
     }
 
@@ -67,11 +72,47 @@ class SpeedImpl : Speed, ConverterDefault {
     }
 
     override fun toKilometerPerHour(fromKey: String, input: Double): Double {
-        TODO("Not yet implemented")
+        return when (fromKey) {
+            "mile_per_hour" -> {
+                input * 1.609
+            }
+
+            "foot_per_second" -> {
+                input * 1.097
+            }
+
+            "meter_per_second" -> {
+                input * 3.6
+            }
+
+            "knot" -> {
+                input * 1.852
+            }
+
+            else -> 0.0
+        }
     }
 
     override fun toMeterPerSecond(fromKey: String, input: Double): Double {
-        TODO("Not yet implemented")
+        return when (fromKey) {
+            "mile_per_hour" -> {
+                input / 2.237
+            }
+
+            "foot_per_second" -> {
+                input / 3.281
+            }
+
+            "kilometer_per_hour" -> {
+                input / 3.6
+            }
+
+            "knot" -> {
+                input / 1.944
+            }
+
+            else -> 0.0
+        }
     }
 
     override fun toFootPerSecond(fromKey: String, input: Double): Double {
@@ -97,7 +138,25 @@ class SpeedImpl : Speed, ConverterDefault {
     }
 
     override fun toKnot(fromKey: String, input: Double): Double {
-        TODO("Not yet implemented")
+        return when (fromKey) {
+            "mile_per_hour" -> {
+                input / 1.151
+            }
+
+            "foot_per_second" -> {
+                input / 1.688
+            }
+
+            "meter_per_second" -> {
+                input * 1.944
+            }
+
+            "kilometer_per_hour" -> {
+                input / 1.852
+            }
+
+            else -> 0.0
+        }
     }
 
 }
